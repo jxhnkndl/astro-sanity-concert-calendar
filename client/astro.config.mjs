1156b96b7702e0c6c7cc5650418e05ded417343b
jxhnkndl <jkroyston@gmail.com>
2024-03-22 15:00:35 -0400

    integrate tailwindcss and sanity cms plugins to astro app

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    tailwind(),
+    sanity({
+      projectId: 'ltujpn1b',
+      dataset: 'production',
+      apiVersion: '2024-03-21',
+      useCdn: false,
+    }),
+  ],
+});
