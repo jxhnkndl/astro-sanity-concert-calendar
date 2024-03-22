89a85a022fa95bb7fc8608f4a914acccab8baf31
jxhnkndl <jkroyston@gmail.com>
2024-03-22 15:34:52 -0400

    create header/nav component

--- client/tailwind.config.mjs
+++ client/tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'dark': '#222325',
+			'light': '#F2F2F2'
+		},
 		extend: {},
 	},
 	plugins: [],
