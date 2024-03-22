63a201cab21a778184e2677a1df170fbb0214f84
jxhnkndl <jkroyston@gmail.com>
2024-03-22 15:35:20 -0400

    create settings document for managing overall page data

--- cms/schemaTypes/index.ts
+++ cms/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import artist from './artist';
 import venue from './venue';
-import concert from './concert'
+import concert from './concert';
+import settings from './settings';
 
-export const schemaTypes = [artist, venue, concert];
+export const schemaTypes = [artist, venue, concert, settings];
---
+++ cms/schemaTypes/settings.ts
@@ -0,0 +1,20 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'settings',
+  title: 'Settings',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'pageTitle',
+      title: 'Page Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'navLinks',
+      title: 'Nav Links',
+      type: 'array',
+      of: [{type: 'string'}]
+    })
+  ]
+});
